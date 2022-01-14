be75408fb0d564fa0cd4e64982cece030184ba55
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-01-13 21:46:25 -0600

    added new theme

--- eggs/chicken.gd
+++ eggs/chicken.gd
@@ -2,7 +2,7 @@ extends Node
 
 func new_text(new_text):
 	match new_text:
-		"here comes the boy":
+		"music make me lose control":
 			get_tree().change_scene("res://eggs/sound_test.tscn")
 		"fastest mario":
 			$"/root/SpeedrunTimer".start()
