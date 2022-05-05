ccd050072ce911f27d4de4e3b02613b1b81c2a0d
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-05-04 19:30:57 -0500

    Add wobbly circles to the loading screen so it isn't as empty

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -15,6 +15,10 @@ func _ready():
 	add_child(loading_bar)
 	var _err = $QodotMap.connect("build_progress", loading_bar, "progress")
 	yield($QodotMap, "build_complete")
+	for c in loading_bar.get_children():
+		for c2 in c.get_children():
+			c2.remove_and_skip()
+		c.remove_and_skip()
 	loading_bar.remove_and_skip()
 	var difficulties = $"/root/EventMan".difficulties
 	for animatronic in difficulties.keys():
