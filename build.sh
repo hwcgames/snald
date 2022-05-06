52122af73b40f733df0cea443cf40ce8227bf801
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-05-05 20:10:42 -0500

    bruh moment

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@ mkdir -v -p build/linux
 flatpak run org.godotengine.Godot -v --export "Linux/X11" build/linux/snald.x86_64
 mkdir -p build/linux/{addons,maps}
 cp -r maps/*.map build/linux/maps
-mkdir -p build/linux/addons/qodot/bin
-cp -r addons/qodot/bin/* build/linux/addons/qodot/bin
+cp -r addons/qodot/bin/x11/* build/linux
