cf640ac17d06663f5ac235d0078604b91f2b1d1f
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-05-29 13:40:31 -0500

    no more cheating

--- entities/button/button.gd
+++ entities/button/button.gd
@@ -2,7 +2,9 @@ extends QodotEntity
 
 
 func _ready():
+	yield(get_parent(), "build_complete")
 	var _err = connect("input_event", self, "on_input_event")
+	_err = connect("mouse_exited", self, "on_mouse_leave")
 	add_to_group("button")
 
 func on_input_event(_camera, event, _click_position, _click_normal, _shape_idx):
@@ -12,3 +14,7 @@ func on_input_event(_camera, event, _click_position, _click_normal, _shape_idx):
 			$"/root/EventMan".circuit_on(properties["circuit"])
 		else:
 			$"/root/EventMan".circuit_off(properties["circuit"])
+
+func on_mouse_leave():
+	if EventMan.circuit(properties["circuit"]):
+		$"/root/EventMan".circuit_off(properties["circuit"])
