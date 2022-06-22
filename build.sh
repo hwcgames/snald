9f5f9f975f26a5771d05f20734dd7a341a455f98
Willow Carlson-Huber <willow.ch@protonmail.com>
2022-06-22 12:11:26 -0500

    Updated build script

--- build.sh
+++ build.sh
@@ -1,6 +1,35 @@
-#!/bin/bash
-mkdir -v -p build/linux
-flatpak run org.godotengine.Godot -v --export "Linux/X11" build/linux/snald.x86_64
-mkdir -p build/linux/{addons,maps}
-cp -r maps/*.map build/linux/maps
-cp -r addons/qodot/bin/x11/* build/linux
+#!/bin/sh
+if [ -z "$SNALD_TARGET" ]; then
+	export SNALD_TARGET=$(uname)
+fi
+which godot > /dev/null || alias godot="flatpak run org.godotengine.Godot"
+
+# Create destination
+export BUILD_DIR=./build/$SNALD_TARGET
+mkdir -p $BUILD_DIR
+# Determine godot output profile
+if [ $SNALD_TARGET == "Darwin" ]; then
+	export GODOT_PROFILE="Mac OSX"
+	export EXTENSION="app"
+elif [ $SNALD_TARGET == "Windows" ]; then
+	export GODOT_PROFILE="Windows Desktop"
+	export EXTENSION="exe"
+else
+	export GODOT_PROFILE="Linux/X11"
+	export EXTENSION="run"
+fi
+# Run godot build
+godot --export "$GODOT_PROFILE" $BUILD_DIR/snald.$EXTENSION
+# Create the maps directory and copy the maps
+mkdir -p $BUILD_DIR/maps
+cp maps/*.map $BUILD_DIR/maps
+# Determine the library extension 
+if [ $SNALD_TARGET == "Darwin" ]; then
+	export EXTENSION="dylib"
+elif [ $SNALD_TARGET == "Windows" ]; then
+	export EXTENSION="dll"
+else
+	export EXTENSION="so"
+fi
+find addons -name *.$EXTENSION -exec cp {} $BUILD_DIR \;
+echo "DONE!"
