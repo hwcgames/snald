b32da4935ac7dd51f62fcd5672eab0b0c9be025f
Willow <willow.ch@protonmail.com>
2022-12-29 21:22:29 -0600

    Added ball pit

---
+++ decorations/ballpit.gd
@@ -0,0 +1,26 @@
+extends Spatial
+
+export var play_now = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if play_now:
+		play()
+		play_now = false;
+	pass
+
+func play():
+	var main_player: AnimationPlayer = $AnimationPlayer
+	for animation_name in main_player.get_animation_list():
+		var sub_player: AnimationPlayer = AnimationPlayer.new();
+		var animation: Animation = main_player.get_animation(animation_name)
+		animation.loop = false
+		sub_player.add_animation(animation_name, animation)
+		add_child(sub_player)
+		sub_player.play(animation_name)
+		sub_player.connect("animation_finished", sub_player, "queue_free")
