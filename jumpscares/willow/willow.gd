254e0f3be480cc2a976363e4d06465b25df38350
Willow <willow.ch@protonmail.com>
2022-12-30 21:26:14 -0600

    Added funny error popups on death screen

--- jumpscares/willow/willow.gd
+++ jumpscares/willow/willow.gd
@@ -1,7 +1,9 @@
 extends BaseJumpscare
 
 func _ready():
+	EventMan.pause = true
 	$AnimationPlayer.play("glitch")
 	yield($AnimationPlayer, "animation_finished")
+	EventMan.pause = false
 	emit_signal("finished")
 	pass
--- jumpscares/willow/willow_dead.gd
+++ jumpscares/willow/willow_dead.gd
@@ -3,5 +3,4 @@ extends Control
 func _ready():
 	$AnimationPlayer.play("go")
 	yield($AnimationPlayer, "animation_finished")
-	EventMan.return_to_title()
 	pass # Replace with function body.
