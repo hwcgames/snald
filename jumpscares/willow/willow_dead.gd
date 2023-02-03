07424c121d7b2c2ee76212655451e074623d2923
Willow <willow.ch@protonmail.com>
2023-02-03 15:22:23 -0600

    Switch to fullscreen during bsod jumpscare

--- jumpscares/willow/willow_dead.gd
+++ jumpscares/willow/willow_dead.gd
@@ -1,6 +1,18 @@
 extends Control
 
+onready var fullscreen = OS.window_fullscreen
+onready var size = get_viewport().size
+
 func _ready():
+	OS.window_fullscreen = true
+	get_viewport().size = OS.get_screen_size()
 	$AnimationPlayer.play("go")
 	yield($AnimationPlayer, "animation_finished")
+	pass # Replace with function body.
+
+
+func _on_ExitDeadButton_pressed():
+	get_viewport().size = size
+	OS.window_fullscreen = fullscreen
+	EventMan.return_to_title()
 	pass # Replace with function body.
