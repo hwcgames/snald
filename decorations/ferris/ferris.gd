0c3e24d02e8c5aa9454e3f19db028c761e238ea8
Willow <willow.ch@protonmail.com>
2023-06-02 08:43:31 -0500

    Added ferris to the office

---
+++ decorations/ferris/ferris.gd
@@ -0,0 +1,20 @@
+extends StaticBody
+
+export var boops: PoolStringArray
+
+func _ready():
+	pass # Replace with function body.
+
+func on_input_event(_camera, event, _click_position, _click_normal, _shape_idx):
+	if CutsceneMan.player_cutscene_mode:
+		return
+	var mouse_click = event as InputEventMouseButton
+	if mouse_click:
+		if mouse_click.pressed:
+			$"../boop".stop()
+			$"../boop".stream = load(boops[rand_range(0, len(boops))])
+			$"../boop".play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
