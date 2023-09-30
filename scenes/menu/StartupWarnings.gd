fd4362355f741de6f6a2b6421687ec8e5d7bd9ac
Willow <willow.ch@protonmail.com>
2023-09-30 12:51:33 -0500

    Add debug startup warning

--- scenes/menu/StartupWarnings.gd
+++ scenes/menu/StartupWarnings.gd
@@ -6,6 +6,8 @@ func _ready():
 		text += 'Cheats have been used\n'
 	if PersistMan.persistent_dict["developer"]:
 		text += 'Developer mode enabled\n'
+	if OS.is_debug_build():
+		text += 'Debug\n'
 	if len(Modloader.mods) > 0:
 		text += str(len(Modloader.mods)) + ' mods loaded:\n'
 		for mod in Modloader.mods:
